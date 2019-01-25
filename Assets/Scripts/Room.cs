26cd44725e8cdb5480469b935adc1b8d7ce0634a
Tomer Blu <Tomer.Blushinsky@Scientificgames.com>
2019-01-25 12:28:26 +0200

    Started room scripts

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public RoomPosition PositionPrefab;
+    public Vector2Int Size;
+    public Vector2 Resolution;
+    public RoomPosition[,] Positions;
+
+    private Transform parent;
+
+    [ContextMenu("Setup")]
+    public void SetupNewRoom()
+    {
+        if (parent != null)
+        {
+            Destroy(parent.gameObject);
+        }
+
+        parent = new GameObject("Positions").transform;
+        parent.SetParent(transform);
+        parent.localPosition = Vector3.zero;
+        
+        Positions = new RoomPosition[Size.x, Size.y];
+
+        for (int x = 0; x < Size.x; x++)
+        {
+            for (int y = 0; y < Size.y; y++)
+            {
+                var position = Instantiate(PositionPrefab, transform);
+                position.Position = new Vector2Int(x, y);
+                
+                position.transform.position = new Vector3(x * Resolution.x, y * Resolution.y, transform.position.z);
+                Positions[x, y] = position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RoomPosition.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RoomPosition : MonoBehaviour
+{
+	public Vector2Int Position;
+	public Item Item;
+
+	public bool IsTaken { get { return Item != null; } }
+}
